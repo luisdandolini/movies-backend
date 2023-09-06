fe34d0241c2b5d4b235dbba81c34b10bb7a18f23
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2023-09-06 16:59:37 -0300

    feat: created method delete movies

--- src/controllers/MoviesController.js
+++ src/controllers/MoviesController.js
@@ -33,6 +33,14 @@ class MoviesController {
       
     }  
   }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    await knex("movies").where({ id }).delete();
+
+    return response.json();
+  }
 }
 
 module.exports = MoviesController;
--- src/routes/movies.routes.js
+++ src/routes/movies.routes.js
@@ -6,5 +6,6 @@ const moviesController = new MoviesController();
 
 moviesRoutes.post('/:user_id', moviesController.create);
 moviesRoutes.get('/:user_id', moviesController.show);
+moviesRoutes.delete('/:id', moviesController.delete);
 
 module.exports = moviesRoutes;
