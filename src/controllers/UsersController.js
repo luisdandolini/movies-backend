3fdf8614258875711c99d8de187d81761de2c0b0
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2023-09-05 21:52:57 -0300

    feat: created method updated user

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -21,6 +21,52 @@ class UsersController {
       return response.status(400).json({ error: error.message });
     }
   }
+  
+  async update(request, response) {
+    const { name, email, password, old_password } = request.body;
+    const { id } = request.params;
+    
+    try {
+      const user = await knex("users").where("id", id).first();
+
+      if(!user) {
+        throw new Error("Usuário não encontrado!");
+      }
+
+      const userWithUpdatedEmail = await knex("users").where("email", email).whereNot("id", user.id).first();
+
+      if(userWithUpdatedEmail) {
+        throw new Error("Este e-mail já está em uso.")
+      }
+
+      if (password && old_password) {
+        const checkOldPassword = await bcrypt.compare(old_password, user.password);
+      
+        if (!checkOldPassword) {
+          throw new Error("A senha antiga não confere.");
+        }
+      
+        const hashedPassword = await bcrypt.hash(password, 8);
+        await knex("users").where("id", id).update({
+          name,
+          email,
+          password: hashedPassword,
+          updated_at: knex.raw('DATETIME("now")')
+        });
+      } else {
+        await knex("users").where("id", id).update({
+          name,
+          email,
+          updated_at: knex.raw('DATETIME("now")')
+        });
+      }
+
+      return response.status(200).json();
+
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
+  }
 }
 
 module.exports = UsersController;
