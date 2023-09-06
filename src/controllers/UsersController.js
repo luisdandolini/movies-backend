19609f2a9134955910a88c37f94a12a2caa66def
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2023-09-05 21:19:19 -0300

    feat: created method post for users

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,25 @@
+const knex = require("../database/knex");
+
+class UsersController {
+  async create(request, response) {
+    const { name, email, password } = request.body;
+
+    try {
+      const checkUserExists = await knex("users").where("email", email).first();
+
+      if(checkUserExists) {
+        throw new Error("Este e-mail já está em uso.");
+      }
+
+      const hanshedPassword = await hash(password, 8);
+
+      await knex("users").insert({ name, email, password: hanshedPassword });
+
+      return response.status(200).json();
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
+  }
+}
+
+module.exports = UsersController;
--- src/server.js
+++ src/server.js
@@ -1,12 +1,30 @@
+require("express-async-errors");
 const express = require("express");
+const AppError = require("./utils/AppError");
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send("Oi")
-})
+const routes = require('./routes');
+app.use(routes);
+
+app.use((error, message, response, next) => {
+  if(error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    })
+  };
+
+  console.log(error);
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  });
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`)
-})
+});
